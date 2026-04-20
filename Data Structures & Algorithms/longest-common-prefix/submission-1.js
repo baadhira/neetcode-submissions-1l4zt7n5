32950bd11d933a279ca39204842c7aec8f3f6259
Badira P P <69342013+baadhira@users.noreply.github.com>
2026-04-20 09:25:30 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        if (strs.length === 0) return "";
+        let prefix = strs[0];
+        for (let i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.slice(0, -1);
+                if (prefix === "") return "";
+            }
+        }
+        return prefix;
+    }
+}
